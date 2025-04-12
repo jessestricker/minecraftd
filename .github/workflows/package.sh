8cdbec5b95b5253a3c37dc5ab0bf85140633ddd6
Jesse Stricker <git@jessestricker.de>
2025-04-12 10:28:44 +0200

    feat: include default config file in package

--- .github/workflows/package.sh
+++ .github/workflows/package.sh
@@ -8,6 +8,7 @@ package_file="minecraftd-$arch.tar.gz"
 
 install -D minecraftd $package_dir/usr/local/bin/minecraftd >&2
 install -D -m 644 minecraft.service $package_dir/usr/local/lib/systemd/system/minecraft.service >&2
+install -D -m 644 minecraftd.toml $package_dir/usr/local/share/minecraftd/minecraftd.toml >&2
 tar --create --gzip --owner 0 --group 0 --file "$package_file" --directory $package_dir . >&2
 tar --list --verbose --file "$package_file" >&2
 
